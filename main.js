acc70bec33f9a53b8f7c51deb90d23369e21ba29
Rpbts <51463854+Rpbts@users.noreply.github.com>
2020-07-19 07:50:04 -0700

    m

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const MongoDB = require("mongodb").MongoClient,
     dbURL = "mongodb://localhost:27017",
-    dbName = "recipe_db";
+    dbName = "recipe_db",
+    mongoose = require("mongoose");
+
+
+
+
 
 MongoDB.connect(dbURL, (error, client) => {
     if (error) throw error;
@@ -11,11 +16,29 @@ MongoDB.connect(dbURL, (error, client) => {
             if (error) throw error;
             console.log(data);
         });
+    db.collection("contacts")
+        .insert({
+            name: "Freddie Mercury",
+            email: "fred@queen.com"
+        }, (error, db) => {
+            if (error) throw error;
+            console.log(db); 2
+        });
 });
 
-db.collection(db.collection("contacts")
-.find()
-.toArray((error, data) => {                    
-  if (error) throw error;
-  console.log(data);                           
-}));
+mongoose.connect(
+    "mongodb://localhost:27017/recipe_db",
+    {useNewUrlParser: true}
+);
+
+const db = mongoose.connection;
+console.log(`db = ${db}`);
+db.once("open", () => {
+    console.log("the event has been fired!");
+});
+
+const subscriberSchema = mongoose.Schema({
+    name : String,
+    email : String,
+    zipCode: Number
+});
