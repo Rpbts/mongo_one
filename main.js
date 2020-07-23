4766b37c0d885a06643a905fa865d8c729e9280b
Rpbts <51463854+Rpbts@users.noreply.github.com>
2020-07-23 07:15:26 -0700

    upd

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 const MongoDB = require("mongodb").MongoClient,
     dbURL = "mongodb://localhost:27017",
-    dbName = "recipe_db",
-    mongoose = require("mongoose");
+    dbName = "recipe_db";
 
-
-
-
-
+/*
 MongoDB.connect(dbURL, (error, client) => {
     if (error) throw error;
     let db = client.db(dbName);
@@ -16,15 +12,19 @@ MongoDB.connect(dbURL, (error, client) => {
             if (error) throw error;
             console.log(data);
         });
-    db.collection("contacts")
-        .insert({
-            name: "Freddie Mercury",
-            email: "fred@queen.com"
-        }, (error, db) => {
-            if (error) throw error;
-            console.log(db); 2
-        });
+
+    db.collection("contacts").insert({
+        name: "rodney crowell",
+        email: "rodnet@crowell.net"
+    }, (error, db) => {
+        if (error) throw error;
+        console.log(`inserted record: ` + db);
+    });
+
 });
+*/
+
+const mongoose = require("mongoose");
 
 mongoose.connect(
     "mongodb://localhost:27017/recipe_db",
@@ -32,13 +32,3 @@ mongoose.connect(
 );
 
 const db = mongoose.connection;
-console.log(`db = ${db}`);
-db.once("open", () => {
-    console.log("the event has been fired!");
-});
-
-const subscriberSchema = mongoose.Schema({
-    name : String,
-    email : String,
-    zipCode: Number
-});
