0047142e41afd8d8aa2540497608fdec419be8a1
Rpbts <51463854+Rpbts@users.noreply.github.com>
2020-07-23 14:40:13 -0700

    lesson 16

--- main.js
+++ main.js
@@ -28,7 +28,41 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
     "mongodb://localhost:27017/recipe_db",
-    {useNewUrlParser: true}
+    { useNewUrlParser: true }
 );
 
 const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("The Mongoose Connection has been Opened Succesfulky");
+}
+);
+
+const subscriberSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    zipCode: Number
+});
+
+const Subscriber = mongoose.model("Subscriber", subscriberSchema);
+
+var subscriber1 = new Subscriber({
+    name: "Lyle Lovett",
+    email: "lyle@lovett.org"
+});
+
+subscriber1.save((error, savedDocument) => {
+    if (error) throw error;
+    console.log(savedDocument);
+});
+
+Subscriber.create(
+    {
+        name: "shawn",
+        email: "scolvin@comcast.net"
+    },
+    function (error, savedDocument) {
+        if (error) throw (error);
+        console.log(savedDocument);
+    }
+);
