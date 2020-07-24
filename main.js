72fc4638db57ea412d6e9f5458a3fd2280457d0c
Rpbts <51463854+Rpbts@users.noreply.github.com>
2020-07-24 08:50:12 -0700

    end 16

--- main.js
+++ main.js
@@ -2,29 +2,7 @@ const MongoDB = require("mongodb").MongoClient,
     dbURL = "mongodb://localhost:27017",
     dbName = "recipe_db";
 
-/*
-MongoDB.connect(dbURL, (error, client) => {
-    if (error) throw error;
-    let db = client.db(dbName);
-    db.collection("contacts")
-        .find()
-        .toArray((error, data) => {
-            if (error) throw error;
-            console.log(data);
-        });
-
-    db.collection("contacts").insert({
-        name: "rodney crowell",
-        email: "rodnet@crowell.net"
-    }, (error, db) => {
-        if (error) throw error;
-        console.log(`inserted record: ` + db);
-    });
-
-});
-*/
-
-const mongoose = require("mongoose");
+const Subscriber = require("./models/subscriber.js");
 
 mongoose.connect(
     "mongodb://localhost:27017/recipe_db",
@@ -37,14 +15,6 @@ db.once("open", () => {
     console.log("The Mongoose Connection has been Opened Succesfulky");
 }
 );
-
-const subscriberSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    zipCode: Number
-});
-
-const Subscriber = mongoose.model("Subscriber", subscriberSchema);
 
 var subscriber1 = new Subscriber({
     name: "Lyle Lovett",
@@ -66,3 +36,10 @@ Subscriber.create(
         console.log(savedDocument);
     }
 );
+
+var myQuery = Subscriber.findOne({ name: "randy" }).where("email", /randy/);
+myQuery.exec((error, data) => {
+    if (data) console.log(`data.name: `+ data.name);
+}
+);
+
